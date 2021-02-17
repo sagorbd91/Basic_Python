ee82cadb07f8dfe5e4ba43f4f14ceac5d457d918
sagor/kumar <sagor.ghosh@dal.ca>
2021-02-17 03:22:47 +0000

    basic 10 things

---
+++ myfile.py
@@ -0,0 +1,47 @@
+# Literals with format
+x=input("Enter a number:")
+y=input("Enter a number:")
+z = x/y
+print(format(z,".2f"))
+
+#===========================
+
+# Justification with format
+
+print(format("hello",">30"))
+print(format("hello", "<40"))
+print(format("helo", "^30"))
+
+#=============================
+
+print(help('keywords'))
+
+# control charecter
+
+print("Hello \n welcome to sagor's class")
+
+# Implicit line joining method
+
+print("hello"
+        "world")
+
+
+#=============================
+
+#membership Operators
+
+taken = 10
+if taken in [12,8,6,5,8,9]:
+    print"here is", taken
+
+elif taken>z:
+    print("taken is greater than z")
+
+elif taken not in [10,2,34,6]:
+    print("taken is in the list")
+else:
+    print("z is greater than taken")
+
+
+# ==============================
+
