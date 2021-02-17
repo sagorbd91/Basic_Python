a0d504f722946b7f3654f788c80491bee877b315
sagor/kumar <sagor.ghosh@dal.ca>
2021-02-17 03:23:11 +0000

    if_else ready

---
+++ if_else.py
@@ -0,0 +1,24 @@
+# sequential condition
+var=int(input("enter a interger value"))
+if var<10:
+    print("value is less than 10")
+else:
+    print("value is greater than 10")
+
+
+# while loop
+looping_var =int(input("enter a integer value"))
+while looping_var<10:
+    print(looping_var)
+    looping_var=looping_var+1
+
+#=====================================
+
+# this program is for checking marks
+
+marks = input("Enter your marks in maths:")
+if marks>=35:
+    print("you got a passing grades")
+else:
+    print("sorry you for failed")
+
